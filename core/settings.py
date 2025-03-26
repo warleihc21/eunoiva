40a8d3b8461681131319f2c6b1ee25b7ae49c3d2
warleihc21 <warleihc21@gmail.com>
2025-03-25 21:57:05 -0300

    Recuperação debug true

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g+1)9+i%t$4(+va(+kbk6f_f83f$cs31fj0(s%zq0bcl(%)dxy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
